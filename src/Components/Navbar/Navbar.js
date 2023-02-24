66a43d161671a68ea9debf1d8473d3309916a229
Sandeep <sandeep@bepoj.com>
2023-02-24 19:50:38 +0530

    working on build failed issue

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,11 +6,11 @@ function Navbar() {
     <center>
         <table width={1024} border={0} cellPadding={0} cellSpacing={0}>
           <tbody><tr>
-              <td width={120} height={47}><a href="http://www.hep2go.com/index_b.php?userRef=gciaake"><img src="https://www.hep2go.com/images/medium-logos/logo-bk.jpg" width={120} height={47} border={0} /></a></td>
-              <td width={120}><a href="http://www.hep2go.com/exercises.php?userRef=gciaake" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image4','','https://www.hep2go.com/buttons/standard_over_bbb.jpg',1)"><img src="https://www.hep2go.com/buttons/standard_bbb.jpg" name="Image4" width={120} height={47} border={0} id="Image4" /></a></td>
-              <td width={120}><a href="http://www.hep2go.com/my_routines/my_routines_private.php?userRef=gciaake" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image5','','https://www.hep2go.com/buttons/exs_ball_over_bbb.jpg',1)"><img src="https://www.hep2go.com/buttons/exs_ball_bbb.jpg" name="Image5" width={120} height={47} border={0} id="Image5" /></a></td>
-              <td width={120}><img src="https://www.hep2go.com/buttons/quick_bbb.jpg" width={120} height={47} /></td>
-              <td width={67} align="left" bgcolor="#000000"><img src="https://www.hep2go.com/buttons/search_bbb.jpg" width={60} height={47} /></td>
+              <td width={120} height={47}><a href="http://www.hep2go.com/index_b.php?userRef=gciaake"><img src="https://www.hep2go.com/images/medium-logos/logo-bk.jpg" alt="" width={120} height={47} border={0} /></a></td>
+              <td width={120}><a href="http://www.hep2go.com/exercises.php?userRef=gciaake"><img src="https://www.hep2go.com/buttons/standard_bbb.jpg" alt="" name="Image4" width={120} height={47} border={0} id="Image4" /></a></td>
+              <td width={120}><a href="http://www.hep2go.com/my_routines/my_routines_private.php?userRef=gciaake" ><img src="https://www.hep2go.com/buttons/exs_ball_bbb.jpg" alt="" name="Image5" width={120} height={47} border={0} id="Image5" /></a></td>
+              <td width={120}><img src="https://www.hep2go.com/buttons/quick_bbb.jpg" alt="" width={120} height={47} /></td>
+              <td width={67} align="left" bgcolor="#000000"><img src="https://www.hep2go.com/buttons/search_bbb.jpg" alt="" width={60} height={47} /></td>
               <td width={205} background="https://www.hep2go.com/buttons/search_end_bbb.jpg" bgcolor="#000000" style={{paddingBottom: '3px'}}>
                 <input id="f_search_kind" name="f_search_kind" type="hidden" defaultValue="exs" />
                 <div id="searchBoxDiv" className="searchBox" style={{width: '200px', padding: '0px', margin: '0px', paddingLeft: '0px'}}>
