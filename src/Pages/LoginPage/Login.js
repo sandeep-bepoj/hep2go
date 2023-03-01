0d4c588c94c29119877720a10e47f34c5d1fcc1e
Sandeep <sandeep@bepoj.com>
2023-03-01 19:47:15 +0530

    fixing conflicts

--- src/Pages/LoginPage/Login.js
+++ src/Pages/LoginPage/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { LoginIm, Signup, Logo } from "../.././Assets";
+import { LoginIm, Logo } from "../.././Assets";
 import { Container } from "@mui/material";
 
 function Login() {
